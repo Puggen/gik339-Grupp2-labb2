dc06097010d39ebdd254be0c630bd5e146046f0d
Puggen <112873174+Puggen@users.noreply.github.com>
2023-12-13 23:24:40 +0100

    container-xl

--- server/client/script.js
+++ server/client/script.js
@@ -2,8 +2,13 @@ const url = "http://localhost:3000";
 const section = document.getElementById("app");
 const ul = document.createElement("ul")
 
+
 ul.classList.add("list-group")
+section.classList.add("container-xl")
 section.appendChild(ul);
+
+
+
 
 
 
@@ -14,7 +19,7 @@ fetch(url + '/users')
     .then(data => {
       console.log(data)
       data.forEach(user => {
-        ul.innerHTML += `<li class="list-group-item" style="background: linear-gradient(270deg, rgba(0,35,36,0.9) 0%, ${user.color});">${user.firstName} ${user.lastName} <br> ${user.username}</li>`;
+        ul.innerHTML += `<li class="list-group-item" style="background: linear-gradient(270deg, rgba(0,35,36,0.9) 10%, ${user.color});">${user.firstName} ${user.lastName} <br> ${user.username}</li>`;
       })
 });
 
